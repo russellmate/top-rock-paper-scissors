c63d7012712488adb6fd21238e917c40b8b23bce
Russell <russelldnb@gmail.com>
2022-02-15 21:06:09 +0000

    updated ui

--- script.js
+++ script.js
@@ -3,27 +3,86 @@ function computerPlay() {
 	const choice = rps[Math.floor(Math.random() * rps.length)];
 	return choice;
 }
+const body = document.querySelector('body');
+body.setAttribute(
+	'style',
+	'height: 100vh; display: flex; justify-content: center; align-items: center; flex-direction: column'
+);
+const results = document.getElementById('results');
+results.setAttribute(
+	'style',
+	'width: 50vw; display: flex; flex-direction: column; align-items: center;'
+);
+let userScore = document.getElementById('user-score');
+userScore.setAttribute('style', 'text-align: center;');
+let computerScore = document.getElementById('computer-score');
+let choices = document.getElementById('choices');
+let finalResult = document.getElementById('final-result');
+let userPoints = 1;
+let computerPoints = 1;
 
 function oneRound(playerSelection, computerSelection) {
+	if (playerSelection == computerSelection) {
+		return (choices.textContent =
+			"It's a draw! You chose " +
+			playerSelection +
+			' and the computer chose ' +
+			computerSelection);
+	}
+
 	if (
 		(playerSelection == 'rock' && computerSelection == 'scissors') ||
 		(playerSelection == 'paper' && computerSelection == 'rock') ||
 		(playerSelection == 'scissors' && computerSelection == 'paper')
 	) {
-		return 'You win! ' + playerSelection + ' beats ' + computerSelection;
+		userScore.textContent = userPoints++;
+		choices.textContent =
+			'You win! You chose ' +
+			playerSelection +
+			' and the computer chose ' +
+			computerSelection;
 	} else {
-		return 'You lose!';
+		computerScore.textContent = computerPoints++;
+		choices.textContent =
+			'You lose! You chose ' +
+			playerSelection +
+			' and the computer chose ' +
+			computerSelection;
 	}
-}
-function game() {
-	for (i = 0; i < 5; i++) {
-		console.log(
-			oneRound(
-				prompt('Rock, paper or scissors?').toLowerCase(),
-				computerPlay()
-			)
-		);
+
+	if (userPoints == 6) {
+		finalResult.textContent = 'You are the winner!';
+	} else if (computerPoints == 6) {
+		finalResult.textContent = 'The computer is the winner!';
+	} else {
+		return;
 	}
 }
 
-game();
+const rock = document
+	.getElementById('rock')
+	.addEventListener('click', function () {
+		if (userPoints == 6 || computerPoints == 6) {
+			return;
+		} else {
+			oneRound('rock', computerPlay());
+		}
+	});
+const paper = document
+	.getElementById('paper')
+	.addEventListener('click', function () {
+		if (userPoints == 6 || computerPoints == 6) {
+			return;
+		} else {
+			oneRound('paper', computerPlay());
+		}
+	});
+const scissors = document
+	.getElementById('scissors')
+	.addEventListener('click', function () {
+		if (userPoints == 6 || computerPoints == 6) {
+			return;
+		} else {
+			oneRound('scissors', computerPlay());
+		}
+	});
