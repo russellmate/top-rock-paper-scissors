54e820318de2d9dec078c9b3644a177261dc5098
Russell <russelldnb@gmail.com>
2023-01-10 21:00:52 +0000

    version 2 update

--- script.js
+++ script.js
@@ -1,88 +1,61 @@
-function computerPlay() {
-	const rps = ['rock', 'paper', 'scissors'];
-	const choice = rps[Math.floor(Math.random() * rps.length)];
-	return choice;
+const choiceText = document.getElementById('choices');
+const userScore = document.getElementById('user-score');
+const computerScore = document.getElementById('computer-score');
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+let uS = 0;
+let cS = 0;
+
+function getComputerChoice() {
+	let choice = ['rock', 'paper', 'scissors'];
+	let random = choice[Math.floor(Math.random() * choice.length)];
+	return random;
 }
-const body = document.querySelector('body');
-body.setAttribute(
-	'style',
-	'height: 100vh; display: flex; justify-content: center; align-items: center; flex-direction: column'
-);
-const results = document.getElementById('results');
-results.setAttribute(
-	'style',
-	'width: 50vw; display: flex; flex-direction: column; align-items: center;'
-);
-let userScore = document.getElementById('user-score');
-userScore.setAttribute('style', 'text-align: center;');
-let computerScore = document.getElementById('computer-score');
-let choices = document.getElementById('choices');
-let finalResult = document.getElementById('final-result');
-let userPoints = 1;
-let computerPoints = 1;
 
-function oneRound(playerSelection, computerSelection) {
-	if (playerSelection == computerSelection) {
-		return (choices.textContent =
-			"It's a draw! You chose " +
-			playerSelection +
-			' and the computer chose ' +
-			computerSelection);
-	}
+function getPlayerChoice() {
+	rock.addEventListener('onclick', () => {
+		return 'rock';
+	});
+	paper.addEventListener('onclick', () => {
+		return 'paper';
+	});
+	scissors.addEventListener('onclick', () => {
+		return 'scissors';
+	});
+}
 
+let computerChoice = getComputerChoice();
+let playerChoice = getPlayerChoice();
+
+function round(playerChoice, computerChoice) {
 	if (
-		(playerSelection == 'rock' && computerSelection == 'scissors') ||
-		(playerSelection == 'paper' && computerSelection == 'rock') ||
-		(playerSelection == 'scissors' && computerSelection == 'paper')
+		(playerChoice === 'rock' && computerChoice === 'paper') ||
+		(playerChoice === 'paper' && computerChoice === 'scissors') ||
+		(playerChoice === 'scissors' && computerChoice === 'rock')
 	) {
-		userScore.textContent = userPoints++;
-		choices.textContent =
-			'You win! You chose ' +
-			playerSelection +
-			' and the computer chose ' +
-			computerSelection;
+		choiceText.textContent = `You lose! You chose ${playerChoice} & the computer chose ${computerChoice}`;
+		cS++;
+		computerScore.textContent = cS;
+	} else if (
+		(playerChoice === 'paper' && computerChoice === 'rock') ||
+		(playerChoice === 'rock' && computerChoice === 'scissors') ||
+		(playerChoice === 'scissors' && computerChoice === 'paper')
+	) {
+		choiceText.textContent = `You win! You chose ${playerChoice} & the computer chose ${computerChoice}`;
+		uS++;
+		userScore.textContent = uS;
 	} else {
-		computerScore.textContent = computerPoints++;
-		choices.textContent =
-			'You lose! You chose ' +
-			playerSelection +
-			' and the computer chose ' +
-			computerSelection;
-	}
-
-	if (userPoints == 6) {
-		finalResult.textContent = 'You are the winner!';
-	} else if (computerPoints == 6) {
-		finalResult.textContent = 'The computer is the winner!';
-	} else {
-		return;
+		choiceText.textContent = `Draw! You chose ${playerChoice} & the computer chose ${computerChoice}`;
 	}
 }
 
-const rock = document
-	.getElementById('rock')
-	.addEventListener('click', function () {
-		if (userPoints == 6 || computerPoints == 6) {
-			return;
-		} else {
-			oneRound('rock', computerPlay());
-		}
-	});
-const paper = document
-	.getElementById('paper')
-	.addEventListener('click', function () {
-		if (userPoints == 6 || computerPoints == 6) {
-			return;
-		} else {
-			oneRound('paper', computerPlay());
-		}
-	});
-const scissors = document
-	.getElementById('scissors')
-	.addEventListener('click', function () {
-		if (userPoints == 6 || computerPoints == 6) {
-			return;
-		} else {
-			oneRound('scissors', computerPlay());
-		}
-	});
+round(playerChoice, computerChoice);
+
+//function game() {
+//	for (let i = 0; i < 5; i++) {
+//		round(playerChoice, computerChoice);
+//	}
+//}
+//
+//game();
